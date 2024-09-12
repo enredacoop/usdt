6cd538b7b16c754717d6b715a46516970f714956
Diego Calvo <diego.calvo@enreda.coop>
2024-09-12 15:18:33 +0200

    add server.mjs

---
+++ packages/frontend/server.mjs
@@ -0,0 +1,19 @@
+import express from "express";
+import path, { join, resolve } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(resolve(__dirname, "dist", "index.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
