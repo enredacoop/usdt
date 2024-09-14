5d1c3881ccee3f51e945c586191564a7000b497e
Diego Calvo <diego.calvo@enreda.coop>
2024-09-14 02:07:45 +0200

    add urlencoded

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -14,6 +14,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ limit: '100mb' }));
 app.use(router);
 app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
