a1ed88d8a8a0c4f688f0e325f2f58db1aa4b9906
Diego Calvo <diego.calvo@enreda.coop>
2025-01-27 13:34:51 +0100

    update description of the project

--- packages/frontend/src/pages/Home/index.tsx
+++ packages/frontend/src/pages/Home/index.tsx
@@ -6,6 +6,8 @@ import upoImg from "../../assets/img/upo.png";
 import vilniausImg from "../../assets/img/vilniaus.jpeg";
 import savoniaImg from "../../assets/img/savonia.png";
 import openodsImg from "../../assets/img/openods.jpeg";
+import table1 from "../../assets/img/table1.png";
+import table2 from "../../assets/img/table2.png";
 
 export default function Home() {
   return (
@@ -38,37 +40,94 @@ export default function Home() {
           <img className="infosection__image" src={amicoImg} alt="" />
           <div className="infosection__content">
             <p>
-              Lorem ipsum dolor sit amet consectetur. Elementum quis massa
-              auctor lectus pharetra ullamcorper maecenas vitae. Arcu tincidunt
-              gravida quis ornare montes ut dictum quis. Tortor viverra
-              ultricies ut eu. Amet ut in aliquam commodo scelerisque. Dictum
-              volutpat pellentesque integer enim placerat faucibus sagittis
-              phasellus euismod. Tellus pretium blandit viverra malesuada
-              molestie euismod. Dapibus feugiat aliquet morbi amet ut non
-              facilisis feugiat. Mauris vulputate egestas lorem praesent.
+            The &quot;Intelligent Target Locator: AI {2030}&quot; (ITL) is a data analytics tool
+powered by AI that enables the recognition and assessment of the direct or
+indirect relationship between any document and each of the 169 targets of the
+17 SDGs. In this way, a cutting-edge tool is made available to higher education
+institutions and third parties, allowing them to quantify the alignment of any
+scenario (curriculum, course, project, publication, activity, etc.) with the 2030
+Agenda..
             </p>
             <p>
-              Integer donec rhoncus massa ornare lectus adipiscing quis neque
-              mauris. Sed amet lacus sed velit dictum luctus et. Ut etiam
-              faucibus sed ipsum risus neque aliquet amet mattis. Quis enim duis
-              ac tempor. Donec ac quis egestas vitae ut commodo. Sed egestas
-              vitae a id amet et ut facilisis. Varius scelerisque et erat et
-              hendrerit odio dui non. Cras integer commodo donec lectus risus
-              nibh morbi pharetra. Sociis pulvinar viverra posuere eu mattis et
-              id sed. Nisl dolor mattis molestie egestas turpis in dictum turpis
-              sit.
+            Through this portal, anyone can submit a document (PDF) to ITL for analysis,
+            and within a few minutes, they will receive an email with the results report..
             </p>
             <p>
-              Consequat feugiat amet rutrum aenean commodo lacus rutrum. Auctor
-              sed nunc et mollis. Vel in odio in dictumst eget adipiscing
-              commodo lacus orci. Morbi egestas cras sed pretium mi.
-              Pellentesque sem congue in velit nec ut. In tellus eu consectetur
-              sagittis viverra nulla sed ultrices. At id dignissim in magna
-              imperdiet at augue et. In dictum scelerisque fringilla dis orci
-              duis. Proin faucibus quis in ut mi sollicitudin sem. Vitae sit
-              massa fermentum quis donec proin in. Augue leo quam quam ut
-              aliquam tempor tellus risus elit. Nunc in condimentum eget mi quis
-              commodo quisque et lectus. Phasellus turpis bibendum pulvinar.
+            ITL employs an innovative methodology developed by the DATA ANALYTICS
+SCIENCE &amp; ENGINEERING research group (TIC 200) at the Universidad Pablo
+de Olavide in Seville, Spain. This methodology combines various Machine
+Learning techniques to quantify the affinity of the input text with each of the
+SDG targets..
+            </p>
+            <h3>How to use?</h3>
+            <p>
+            <strong>Provide the Information</strong>
+            <ol>
+              <li>Specify the location on your computer where the document you want
+              to analyze is stored.</li>
+              <li>Enter the email address where you want to receive the link to the
+              results.</li>
+              <li>Provide the document title or any information that will help you
+              identify the document when you receive the results.</li>
+              <li>Accept the terms and conditions of use.</li>
+              <li>Click the ”Send&quot; button.</li>
+            </ol>
+            <strong>Confirm Your Email</strong>
+            <ol>
+              <li>You will need to confirm the email address provided by entering the
+              verification code sent to that same email address.</li>
+              <li>Click &quot;Verify.&quot;</li>
+            </ol>
+            <strong>Receive the Results</strong>
+            <ol>
+              <li>You will receive an email with a link to the results</li>
+              <li>Click on the link and you will see the results and download them.</li>
+            </ol>
+            </p>
+            <h3>What results does it return?</h3>
+            <p>
+           <strong>Affinity</strong> is a numerical measure that quantifies the relationship between a text
+and each target individually. It is based on the similarity of the words and
+combinations of words that appear in the target texts and the documents being
+analyzed. Affinity is, therefore, sensitive to the properties of the reference texts
+
+(the targets, in this case), meaning there is no single reference affinity value
+applicable to all targets; instead, each target has a specific objective affinity
+value to be reached.
+            </p>  
+            <p>
+            Thus, ITL provides three affinity values for the document with respect to each
+target:
+            <ul>
+              <li>
+              <strong>Absolute Affinity Value</strong>: The affinity value calculated by the tool for the
+              reference target.
+              </li>
+              <li>
+              <strong>Relative Affinity Value</strong>: The ratio of the absolute affinity value to the
+reference affinity value. The closer this value is to 1, the greater the
+affinity of the text with the specific target.
+              </li>
+<li><strong>Affinity Value</strong>: The percentage affinity value of the document for each
+target. In other words, it identifies which targets are most related to the
+document, regardless of the magnitude of the relationship.</li>
+            </ul>
+            </p> 
+            <h3>Downloading numerical results</h3>
+            <p>
+            The tool provides two options to download the numerical results of the analysis:
+              <ul>
+                <li>
+                Document Affinity: Returns a file (CSV) with the affinity values of the whole
+                document for each target.
+                </li>
+              </ul>
+              <img className="infosection__descriptionImage" src={table1} alt="Affinity values per target" />
+              <ul><li>
+              Affinity Table: Returns a file (CSV) with all the absolute affinity values for each
+              goal and target for every sentence in the document.
+              </li></ul>
+              <img className="infosection__descriptionImage" src={table2} alt="Affinity values per target" />
             </p>
           </div>
         </div>
